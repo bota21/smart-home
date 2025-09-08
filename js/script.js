f7341d03ab77876c668b940113c9776a8dbec3b6
Botagoz Aimkulova <aimkulow@bk.ru>
2025-09-08 20:33:29 +0500

    fished yandex tab

--- js/script.js
+++ js/script.js
@@ -14,18 +14,18 @@
     });
 
 
-    $('.wrapper_for_smart_home_item').hide();
-    $('.wrapper_for_smart_home_item[data-tab="0"]').show();
-    $('.tabs_for_smart_home_item[data-tab="0"]').addClass('tab_item_active');
+    $('.wrapper_for_smart_home_item_1').hide();
+    $('.wrapper_for_smart_home_item_1[data-tab="01"]').show();
+    $('.tabs_for_smart_home_item_1[data-tab="01"]').addClass('tab_item_active');
   
-    $('.tabs_for_smart_home_item').click(function () {
+    $('.tabs_for_smart_home_item_1').click(function () {
       const tabIndex = $(this).data('tab');
   
-      $('.tabs_for_smart_home_item').removeClass('tab_item_active');
+      $('.tabs_for_smart_home_item_1').removeClass('tab_item_active');
       $(this).addClass('tab_item_active');
   
-      $('.wrapper_for_smart_home_item').hide();
-      $('.wrapper_for_smart_home_item[data-tab="' + tabIndex + '"]').fadeIn();
+      $('.wrapper_for_smart_home_item_1').hide();
+      $('.wrapper_for_smart_home_item_1[data-tab="' + tabIndex + '"]').fadeIn();
     });
 
 
